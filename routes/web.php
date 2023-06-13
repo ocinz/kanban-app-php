14b301684cddab843fb20b08f7b8c6653faea117
ocinz <ahmadhafid28632@gmail.com>
2023-06-14 01:54:09 +0700

    Develop Edit Task View

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-})->name('home'); // name ditambahkan
+})->name('home');
 
-Route::get('/tasks/', [TaskController::class, 'index'])->name('tasks.index'); // name ditambahkan
+Route::prefix('tasks')
+    ->name('tasks.')
+    ->controller(TaskController::class)
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('{id}/edit', 'edit')->name('edit');
+    });
